7c51449f88999878bc324f41b8ca2b84cba26f09
md.ashrafuzzaman <md.ashrafuzzaman@amiprobashi.com>
2026-04-15 14:45:39 +0600

    product script git s3 push fixed

--- devops/scripts/deploy-production.sh
+++ devops/scripts/deploy-production.sh
@@ -3,21 +3,24 @@
 
 # Runs inside Bitbucket pipelines. Creates a tar bundle for our project
 # and stores it in AWS S3 Bucket. Then creates a new deployment using
-# AWS CodeDeploy. The files are listed inside the bundle.conf file.
+# AWS CodeDeploy.
 
-# Make sure you set the following Repository Variables inside Bitbucket [Repository Settings].
+# --- Configuration ---
+# Ensure these are set in Bitbucket Repository Settings:
+# S3_BUCKET (e.g., laravel12devops)
+# APPLICATION_NAME
+# DEPLOYMENT_CONFIG_NAME
+# DEPLOYMENT_GROUP_NAME
 
-# S3_BUCKET
-# APPLICATION_NAME = CodeDeploy application name
-# DEPLOYMENT_CONFIG_NAME = CodeDeploy > Deployment Configuration
-# DEPLOYMENT_GROUP_NAME = your CodeDeploy > Deployments > Deployment Group Column
+HASH=$(git rev-parse --short HEAD)
+BUNDLE="$HASH.tar.gz"
 
-HASH=`git rev-parse --short HEAD`
-BUNDLE=$HASH.tar.gz
-S3_BUCKET_ENDPOINT="s3://$S3_BUCKET/bundles/"
+# Clean up any old local bundles
+rm -f bundle-*.tar.gz
 
-rm -rf bundle-*.tar.gz
+echo "[-] Creating bundle: $BUNDLE"
 
+# Create the archive based on bundle.conf
 tar \
   --exclude='*.git*' \
   --exclude='storage/logs/*' \
@@ -26,15 +29,21 @@ tar \
   --exclude='artifact/*' \
   --exclude='.styleci.yml' \
   --exclude='.env' \
-  -zcf $BUNDLE -T bundle.conf > /dev/null 2>&1
+  -zcf "$BUNDLE" -T bundle.conf > /dev/null 2>&1
 
-aws s3 cp $BUNDLE $S3_BUCKET_ENDPOINT > /dev/null 2>&1
-echo "[-] Your CodeDeploy S3 endpoint will be: $S3_BUCKET_ENDPOINT$BUNDLE"
+# Upload to S3
+# Note: We use the s3:// protocol here because 'aws s3 cp' requires it.
+echo "[-] Uploading to S3..."
+aws s3 cp "$BUNDLE" "s3://$S3_BUCKET/bundles/$BUNDLE" > /dev/null 2>&1
 
-# https://docs.aws.amazon.com/cli/latest/reference/deploy/create-deployment.html
+echo "[-] Deployment starting for revision: $HASH"
+
+# --- The Fix ---
+# For 'create-deployment', the 'bucket' parameter must be ONLY the name.
+# The 'key' parameter is the path inside that bucket.
 aws deploy create-deployment \
-  --application-name $APPLICATION_NAME \
-  --deployment-config-name $DEPLOYMENT_CONFIG_NAME \
-  --deployment-group-name $DEPLOYMENT_GROUP_NAME \
+  --application-name "$APPLICATION_NAME" \
+  --deployment-config-name "$DEPLOYMENT_CONFIG_NAME" \
+  --deployment-group-name "$DEPLOYMENT_GROUP_NAME" \
   --file-exists-behavior OVERWRITE \
-  --s3-location bucket=$S3_BUCKET_ENDPOINT,bundleType=tgz,key=bundles/$BUNDLE
+  --s3-location bucket="$S3_BUCKET",bundleType=tgz,key="bundles/$BUNDLE"
